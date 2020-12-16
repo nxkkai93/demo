bcb890f206e85f6bb432fff42ee9e0c77397947a
DESKTOP-8LSN08E\KAI <nxk.kai93@gmail.com>
2020-12-16 19:25:12 +0900

    update key

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module Demo
     
   end
 
-  AUTH_HEADER = "Bearer c3193a91945092aec6d175d65fc20cba902693f4"
+  AUTH_HEADER = "Bearer 2c91a0c00e654937b73882fa5b716bb63b275025"
 
 	  HTTP = GraphQL::Client::HTTP.new("https://api.github.com/graphql") do 
 	  #上記には、接続したいAPIのエンドポイントURLを記入
